16ba01359c174ee599cc3a65679c226d94488fe8
Oleh Fidelis <olehfidelis3609@icloud.com>
2025-02-21 07:50:37 +0100

    added some files

---
+++ src/database/init.ts
@@ -0,0 +1,11 @@
+import Db from "./index";
+
+const DbInitailize = async () => {
+    try{
+       await Db.authenticate();
+    }catch(error){
+        console.log("unable to connect to our database ", error)
+    }
+}
+
+export default  DbInitailize();
