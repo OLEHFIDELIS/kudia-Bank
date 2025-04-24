dec062d4ff52f93b1532acde01085709749d2da7
Oleh Fidelis <olehfidelis3609@icloud.com>
2025-04-24 13:43:05 +0100

    worked on processing user data

--- src/controllers/user-controller.ts
+++ src/controllers/user-controller.ts
@@ -1,5 +1,11 @@
-import UserService from "../services/user-service";
+import bcrypt from "bcryptjs"
 import  {Request, Response} from "express";
+import { AccountStatus, EmailStatus, UserRoles } from "../interfaces/enum/user-enum";
+import { IUserCreationBody } from "../interfaces/user-interface";
+import UserService from "../services/user-service";
+import Utility from "../utils/index.utils";
+import { ResponseCode } from "../interfaces/enum/code-enum";
+
 
 class UserController{
     
@@ -8,9 +14,37 @@ class UserController{
         this.userService =  _userService;
     }
 
+    //-- structure the data
+    //-- hash the password 
+    //-- create user
+    //-- generate a token for verification 
+    //-- send the welcome/verification email
+
     async register(req: Request, res: Response){
+
         try {
-            res.send({ message: "Registration successful" })
+            const params = {...req.body};
+            const newUser = {
+                firstname: params.firstname,
+                lastname: params.lastname,
+                email: params.email,
+                username: params.email.split("@")[0],
+                password: params.Password,
+                role: UserRoles.CUSTOMER,
+                isEmailVerified: EmailStatus.NO_TVERIFIED,
+                accountStatus: AccountStatus.ACTIVE,
+            }as  IUserCreationBody;
+            newUser.password = bcrypt.hashSync(newUser.password, 10 );
+
+            let userExist = await this.userService.getUserByField({email: newUser.email});
+
+            if(userExist){
+                return Utility.handleError(res, "Email already exists", ResponseCode.ALREADY_EXIST);
+            };
+
+            let user = await this.userService.createUser(newUser);
+            user.password = "";
+            return Utility.handleSuccess(res, "User registered successfuly", {user}, ResponseCode.SUCCSESS)
         } catch (error) {
             res.send({ message: "Server Error" })
         }
---
+++ src/interfaces/enum/user-enum.ts
@@ -0,0 +1,18 @@
+export const EmailStatus = {
+    VERIFIED: "VERIFIED",
+    NO_TVERIFIED: "NOT_VERIFIED"
+};
+
+export const UserRoles = {
+  ADMIN: "ADMIN",
+  EDITOR: "EDITOR",
+  CUSTOMER: "CUSTOMER",
+
+};
+
+export const AccountStatus = {
+    ACTIVE: "ACTIVE",
+    IN_ACTIVE: "IN_ACTIVE",
+    SUSPENDED: "SUSPENDED",
+    
+}
